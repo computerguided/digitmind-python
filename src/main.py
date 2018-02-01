1cb868bc9b0428495efb2c6a8d60402af8e552ce
Fred Dijkstra <fred@factic.net>
2018-02-01 17:12:11 +0100

    Human and computer player at different difficulty levels

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import random
 
-
-def create_combinations():
-    digits = list(range(0,10))
+def create_combinations(difficulty_level):
+    digits = [1,2,3,4,5,6,7,8,9,0]
+    digits = digits[:difficulty_level+3]
     return [(w, x, y, z)        \
         for w in digits         \
         for x in digits         \
@@ -12,43 +12,79 @@ def create_combinations():
         and x not in (y,z)      \
         and y != z ]
 
-def determine_score(combination, code):
+def determine_score_alternative(combination, code):
+    score = {'correct position':0, 'wrong position':0}
     pairwise = zip(combination, code)
-    num_correct_position = len([idx for idx, pair in enumerate(pairwise) \
-                                if pair[0] == pair[1]])
-
-    num_wrong_position = len(set(combination).intersection(set(code)))-num_correct_position
-    score = {'correct position':num_correct_position, 'wrong position':num_wrong_position}
+    score['correct position'] = sum(i for i, p in enumerate(pairwise) if p[0] == [1])
+    score['wrong position'] = len(set(combination).intersection(set(code))) - \
+                              score['correct position']
     return score
+
+def determine_score(combination, code):
+    score = {'correct position':0, 'wrong position':0}
+    for i in range(len(combination)):
+        if combination[i] == code[i]:
+            score['correct position'] += 1
+        elif combination[i] in code:
+            score['wrong position'] += 1
+    return score
+
+def determine_score_difference(combination, code):
+    return sum(abs(combination[i] - code[i]) for i in range(len(combination)))
     
 def process_score(combinations_left, tried_combination, score):
     max_index = len(combinations_left)-1
     for i, combination in enumerate(reversed(combinations_left)):
         if score != determine_score(combination, tried_combination):
             del combinations_left[max_index-i]
+            
+def process_score_alternative(combinations_left, tried_combination, score):
+    max_index = len(combinations_left)-1
+    for i, combination in enumerate(reversed(combinations_left)):
+        if score != determine_score(combination, tried_combination):
+            del combinations_left[max_index-i]
 
-def do_computer_move(combinations_left):
-    guess = random.choice(combinations_left)
+def do_computer_move(combinations_left, difficulty_level):
     
+    # Input error handling
+    if len(combinations_left) == 0:
+        print("You probably made an error in specifying the score for one of my guesses!")
+        print("Let's start over..")
+        combinations_left.extend(create_combinations(difficulty_level))
+
+    guess = random.choice(combinations_left)
     print('My guess:', guess)
 
     score['correct position'] = int(input('How many on the correct position? '))
     
     if score['correct position'] != 4:
         score['wrong position'] = int(input('How many on the wrong position? '))
-        process_score(combinations, guess, score)
+        process_score(combinations_left, guess, score)
     
     return score
-    
 
-combinations = create_combinations()
 
-code  = [5, 4, 8, 1]
+difficulty_level = int(input("Give the difficulty level [1..7]: "))
+
+# Human code breaker
+combinations = create_combinations(difficulty_level)
+code = random.choice(combinations)
+
+print("\nOk, I’ve chosen a code, try to guess it!\n")
 
 score = {'correct position':0, 'wrong position':0}
-
 while score['correct position'] != 4:
-    score = do_computer_move(combinations)
+    guess = [int(c) for c in input("Your next guess: ") ]
+    score = determine_score(code, guess)
+    print(score)
+
+print("\nCorrect! Well done!")
+
+# Computer code breaker
+combinations = create_combinations(difficulty_level)
+score = {'correct position':0, 'wrong position':0}
+while score['correct position'] != 4:
+    score = do_computer_move(combinations, difficulty_level)
         
 print('YES!')
 
